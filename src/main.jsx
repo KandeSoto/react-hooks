d0148484818d746d48ed91cb72f7028f59396530
KandeSoto <Candelario.soto@sparktechs.com>
2024-12-11 18:48:19 -0800

    useEffect

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,7 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
-import { CounterWithCustomHook } from './01-useState/CounterWithCustomHook';
+import { SimpleForm } from './02-useEffect/SimpleForm';
+// import { CounterWithCustomHook } from './01-useState/CounterWithCustomHook';
 // import { CounterApp } from './01-useState/CounterApp';
 // import { HooksApp } from './HooksApp';
 
@@ -8,6 +9,6 @@ import './index.css'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <CounterWithCustomHook />
+    <SimpleForm />
   </StrictMode>
 )
