ffe6c2be0c9a52b33584cc2fd9269d2a7a9f87b9
KandeSoto <Candelario.soto@sparktechs.com>
2024-12-13 17:54:45 -0800

    UseForm

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,7 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
-import { SimpleForm } from './02-useEffect/SimpleForm';
+import { FormWithCustomHook } from './02-useEffect/FormWithCustomHook';
+// import { SimpleForm } from './02-useEffect/SimpleForm';
 // import { CounterWithCustomHook } from './01-useState/CounterWithCustomHook';
 // import { CounterApp } from './01-useState/CounterApp';
 // import { HooksApp } from './HooksApp';
@@ -9,6 +10,6 @@ import './index.css'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <SimpleForm />
+    <FormWithCustomHook />
   </StrictMode>
 )
