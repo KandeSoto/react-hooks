f66c4ffea8de2ee0790e2b24397553e637453314
KandeSoto <Candelario.soto@sparktechs.com>
2024-12-17 11:46:04 -0800

    custom hook useFetch

---
+++ src/03-examples/MultipleCustomHooks.jsx
@@ -0,0 +1,14 @@
+import { useFetch } from "../hooks"
+
+
+export const MultipleCustomHooks = () => {
+
+  useFetch();
+
+
+
+
+  return (
+    <div>MultipleCustomHooks</div>
+  )
+}
--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,7 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
-import { FormWithCustomHook } from './02-useEffect/FormWithCustomHook';
+import { MultipleCustomHooks } from './03-examples/MultipleCustomHooks';
+// import { FormWithCustomHook } from './02-useEffect/FormWithCustomHook';
 // import { SimpleForm } from './02-useEffect/SimpleForm';
 // import { CounterWithCustomHook } from './01-useState/CounterWithCustomHook';
 // import { CounterApp } from './01-useState/CounterApp';
@@ -10,6 +11,6 @@ import './index.css'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <FormWithCustomHook />
+    <MultipleCustomHooks />
   </StrictMode>
 )
