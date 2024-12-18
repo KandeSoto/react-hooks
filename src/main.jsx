9db5f6bf1ae1055ac5dace6df1226a126cd0b929
KandeSoto <Candelario.soto@sparktechs.com>
2024-12-17 17:25:23 -0800

    useLayaoutEffect

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,7 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
-import { FocusScreen } from './04-useRef/FocusScreen';
+import { Layaout } from './05-useLayaoutEffect/Layaout';
+// import { FocusScreen } from './04-useRef/FocusScreen';
 // import { MultipleCustomHooks } from './03-examples/MultipleCustomHooks';
 // import { FormWithCustomHook } from './02-useEffect/FormWithCustomHook';
 // import { SimpleForm } from './02-useEffect/SimpleForm';
@@ -12,6 +13,6 @@ import './index.css'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <FocusScreen />
+    <Layaout />
   </StrictMode>
 )
