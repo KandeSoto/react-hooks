4dcba7973aec872ea952166567d59a04632b1151
KandeSoto <Candelario.soto@sparktechs.com>
2024-12-20 13:09:11 -0800

    Create Context y userProvider

--- src/09-useContext/MainApp.jsx
+++ src/09-useContext/MainApp.jsx
@@ -3,10 +3,11 @@ import { HomePage } from "./HomePage";
 import { AboutPage } from "./AboutPage";
 import { LoginPage } from "./LoginPage";
 import { Navbar } from "./Navbar";
+import { UserProvider } from "./context/UserProvider";
 
 export const MainApp = () => {
     return (
-        <>
+        <UserProvider>
             <Navbar />
             <hr />
 
@@ -18,6 +19,6 @@ export const MainApp = () => {
                 {/* <Route path="/*" element={<LoginPage />} /> */}
                 <Route path="/*" element={<Navigate to="/about" />} />
             </Routes>
-        </>
+        </UserProvider>
     );
 };
---
+++ src/09-useContext/context/UserProvider.jsx
@@ -0,0 +1,15 @@
+import { UserContext } from "./UserContext";
+
+const user = {
+    id: 1,
+    name: "Kande",
+    email: "kandelario@gmail.com",
+};
+
+export const UserProvider = ({ children }) => {
+    return (
+        <UserContext.Provider value={{ hola: "Mundo", user: user }}>
+            {children}
+        </UserContext.Provider>
+    );
+};
