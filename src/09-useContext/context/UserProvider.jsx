82e7fdba063db3aa833d4118a8902a53b554ff57
KandeSoto <Candelario.soto@sparktechs.com>
2024-12-20 15:38:30 -0800

    useContext

--- src/09-useContext/context/UserProvider.jsx
+++ src/09-useContext/context/UserProvider.jsx
@@ -1,14 +1,18 @@
+import { useState } from "react";
 import { UserContext } from "./UserContext";
 
-const user = {
-    id: 1,
-    name: "Kande",
-    email: "kandelario@gmail.com",
-};
+// const user = {
+//     id: 1,
+//     name: "Kande",
+//     email: "kandelario@gmail.com",
+// };
 
 export const UserProvider = ({ children }) => {
+    const [user, setUser] = useState();
+
     return (
-        <UserContext.Provider value={{ hola: "Mundo", user: user }}>
+        // <UserContext.Provider value={{ hola: "Mundo", user: user }}>
+        <UserContext.Provider value={{ user, setUser }}>
             {children}
         </UserContext.Provider>
     );
